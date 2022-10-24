16f0403ada94ec2276ed1708913fdf607ec17990
raonixx <108378080+raonixx@users.noreply.github.com>
2022-10-23 21:56:02 -0300

    Create http_back_end.js

---
+++ http_back_end.js
@@ -0,0 +1,27 @@
+/*Crie um código JavaScript no back-end utilizando o Node.JS. No código, você deverá elaborar uma ou mais rotas, que podem ser de qualquer tipo (get, put etc). 
+Depois, faça um teste em aplicações de rota, como o Postman ou o Insomnia, para confirmar se o retorno está coerente com o que você programou.
+Observação: os prints, ou o próprio código, devem ser divididos por arquivos. Por exemplo, o código de rotas está em um arquivo diferente do código de conexão.
+*/
+
+//CODIGO
+const porta = 6060
+
+const express = require('express')
+const app = express()
+
+app.get('/user', (req, res, next) => {
+    res.send({
+        usuario1: {
+            nome: "Raoni Xavier",
+            email: "raoni.x@gmail.com"
+        },
+        usuario2: {
+            nome: "Carlos Eduardo",
+            email: "carlosduardo@gmail.com"
+        }
+    }) 
+})
+
+app.listen(porta, () => {
+    console.log(`Servidor executando na porta ${porta}.`)
+})
